01a936485ba9798c43b15a489360d3d1eff3582a
Janne Vanhala <janne.vanhala@gmail.com>
2012-09-25 23:38:44 +0300

    Started working on Storage and File classes.

---
+++ tests/unit/test_core.py
@@ -0,0 +1,78 @@
+from flexmock import flexmock
+
+
+def get_storage_class():
+    from unistorage.core import Storage
+    return Storage
+
+
+def get_file_class():
+    from unistorage.core import File
+    return File
+
+
+def make_storage(*args, **kwargs):
+    Storage = get_storage_class()
+    return Storage(*args, **kwargs)
+
+
+def make_file(*args, **kwargs):
+    File = get_file_class()
+    return File(*args, **kwargs)
+
+def make_storage_with_fake_adapter():
+    adapter = flexmock()
+    storage = make_storage(adapter)
+    return storage
+
+
+class TestStorage(object):
+
+    def test_constructor_sets_adapter(self):
+        adapter = flexmock()
+        storage = make_storage(adapter)
+        assert storage.adapter is adapter
+
+    def test_contains_returns_false_if_key_doesnt_exist(self):
+        storage = make_storage_with_fake_adapter()
+        (storage.adapter
+            .should_receive('exists')
+            .with_args('README.rst')
+            .and_return(False)
+            .once()
+        )
+        assert 'README.rst' not in storage
+
+    def test_contains_returns_true_if_key_exists(self):
+        storage = make_storage_with_fake_adapter()
+        (storage.adapter
+            .should_receive('exists')
+            .with_args('README.rst')
+            .and_return(True)
+            .once()
+        )
+        assert 'README.rst' in storage
+
+    def test_getitem_returns_a_file_with_the_given_key(self):
+        from unistorage import core
+        storage = make_storage_with_fake_adapter()
+        fake_file = flexmock()
+        (flexmock(core)
+            .should_receive('File')
+            .with_args(storage, 'README.rst')
+            .and_return(fake_file)
+            .once()
+        )
+        assert storage['README.rst'] is fake_file
+
+
+class TestFile(object):
+    def test_constructor_sets_storage(self):
+        storage = flexmock()
+        file_ = make_file(storage, 'README.rst')
+        assert file_.storage is storage
+
+    def test_constructor_sets_key(self):
+        storage = flexmock()
+        file_ = make_file(storage, 'README.rst')
+        assert file_.key == 'README.rst'
--- unistorage/core.py
+++ unistorage/core.py
@@ -13,163 +13,25 @@
 
 
 class Storage(object):
+    def __init__(self, adapter):
+        self.adapter = adapter
 
-    def __len__(self):
-        """
-        Return the number of files in this storage.
-        """
-        raise NotImplementedError
-
-    def __iter__(self):
-        """
-        Iterate over the storage yielding each file contained in it.
-        """
-        raise NotImplementedError
-
-    def accessed(self, name):
-        """
-        Return the last accessed time of a file.
-
-        For storage systems that are not able to return the last
-        accessed time this will raise :exc:`NotImplementedError`
-        instead.
-
-        If a file with the given `name` does not exist then a
-        :exc:`storage.exc.FileNotFoundError` exception is raised.
-
-        :argument name: the name of the file whose last accessed time
-          you want to check.
-        :returns: a :class:`datetime.datetime` object
-        """
-        raise NotImplementedError
-
-    def created(self, name):
-        """
-        Return the creation time of a file.
-
-        For storage systems that are not able to return the creation
-        time this will raise :exc:`NotImplementedError` instead.
-
-        If a file with the given `name` does not exist then a
-        :exc:`storage.exc.FileNotFoundError` exception is raised.
-
-        :argument name: the name of the file whose creation time you
-          want to check.
-        :returns: a :class:`datetime.datetime` object
-        """
-        raise NotImplementedError
-
-    def modified(self, name):
-        """
-        Return the last modified time of a file.
-
-        For storage systems that are not able to return the last
-        modified time this will raise :exc:`NotImplementedError`
-        instead.
-
-        If a file with the given `name` does not exist then a
-        :exc:`storage.exc.FileNotFoundError` exception is raised.
-
-        :argument name: the name of the file whose last modified time
-          you want to check.
-        :returns: a :class:`datetime.datetime` object
-        """
-        raise NotImplementedError
-
-    def exists(self, name):
+    def __contains__(self, key):
         """
         Check for the existence of a file.
 
-        :argument name: the name of the file to check for existence
-        :returns: ``True`` if `name` refers to an existing file within
-          this storage, or ``False`` if the name is available for a new
+        :argument key: the key of the file to check for existence
+        :returns: ``True`` if `key` refers to an existing file within
+          this storage, or ``False`` if the key is available for a new
           file.
         """
-        raise NotImplementedError
+        return self.adapter.exists(key)
 
-    def open(self, name, mode='rb'):
-        """
-        Return an :class:`File` instance for an existing file.
-
-        If a file with the given `name` does not exist then a
-        :exc:`storage.exc.FileNotFoundError` exception is raised.
-
-        :argument name: the name of the file to retrieve
-        :returns: a :class:`File` representing the file requested
-        """
-        raise NotImplementedError
-
-    def __str__(self):
-        return self.name
+    def __getitem__(self, key):
+        return File(self, key)
 
 
 class File(object):
-
-    def __init__(self, storage, name):
+    def __init__(self, storage, key):
         self.storage = storage
-        self.name = name
-        self._mode = mode
-
-    def closed(self):
-        """
-        A boolean indicating whether the file is closed.
-        """
-        raise NotImplementedError
-
-    @property
-    def mode(self):
-        """
-        The read/write mode for the file.
-        """
-        return self._mode
-
-    @property
-    def size(self):
-        """
-        The size of this file in bytes.
-        """
-        raise NotImplementedError
-
-    @property
-    def url(self):
-        raise NotImplementedError
-
-    def read(self, num_bytes=None):
-        """
-        Read content from the file.
-
-        :param num_bytes: the number of bytes to read; if not specified
-            the file will be read to the end.
-        """
-        raise NotImplementedError
-
-    def write(self, content):
-        """
-        Write content to the file.
-
-        Depending on the storage system behind the scenes, this content
-        might not be fully committed until :method:`close` is called on
-        the file.
-
-        :param content: a string or `file`-like object to write to the
-            file.
-        """
-        raise NotImplementedError
-
-    def close(self):
-        """
-        Close the file.
-        """
-        raise NotImplementedError
-
-    def __iter__(self):
-        """
-        Iterate over the file yielding one line at a time.
-        """
-        raise NotImplementedError
-
-    def __len__(self):
-        return self.size
-
-    def __str__(self):
-        return self.name
+        self.key = key
