8a8b49e3d2aadb69227c0051bf6cee66108b559e
Janne Vanhala <janne.vanhala@gmail.com>
2012-10-11 21:58:35 +0300

    started implementing amazon s3 adapter

---
+++ tests/unit/adapters/test_amazon.py
@@ -0,0 +1,123 @@
+import os
+
+import pytest
+from flexmock import flexmock
+
+
+def get_adapter_class():
+    from unistorage.adapters.amazon import AmazonS3
+    return AmazonS3
+
+
+def make_adapter(*args, **kwargs):
+    AmazonS3 = get_adapter_class()
+    return AmazonS3(*args, **kwargs)
+
+
+class TestAmazonS3(object):
+    def setup_method(self, method):
+        pytest.importorskip("boto")
+
+    def test_constructor_creates_s3_connection_from_credentials(self):
+        AmazonS3 = get_adapter_class()
+
+        fake_boto = flexmock()
+        (
+            flexmock(AmazonS3)
+            .should_receive('_import_boto')
+            .and_return(fake_boto)
+            .once()
+        )
+
+        fake_connection = flexmock()
+        (
+            fake_boto
+            .should_receive('connect_s3')
+            .with_args('TEST_ID', 'TEST_SECRET')
+            .and_return(fake_connection)
+            .once()
+        )
+        adapter = AmazonS3('TEST_ID', 'TEST_SECRET', 'test-bucket')
+        assert adapter.connection is fake_connection
+
+    def test_contructors_sets_bucket_name(self):
+        adapter = make_adapter('TEST_ID', 'TEST_SECRET', 'test-bucket')
+        assert adapter.bucket_name == 'test-bucket'
+
+    def test_get_or_create_bucket_retrieves_the_bucket_if_it_exists(self):
+        adapter = make_adapter('TEST_ID', 'TEST_SECRET', 'test-bucket')
+        fake_bucket = flexmock()
+        (
+            flexmock(adapter.connection)
+            .should_receive('get_bucket')
+            .with_args('test-bucket')
+            .and_return(fake_bucket)
+            .once()
+        )
+        bucket = adapter._get_or_create_bucket()
+        assert bucket is fake_bucket
+
+    def test_get_or_create_bucket_creates_the_bucket_unless_it_exists(self):
+        from boto.exception import S3ResponseError
+
+        adapter = make_adapter('TEST_ID', 'TEST_SECRET', 'test-bucket')
+        (
+            flexmock(adapter.connection)
+            .should_receive('get_bucket')
+            .with_args('test-bucket')
+            .and_raise(S3ResponseError, 404, 'Not Found', '')
+            .once()
+        )
+        fake_bucket = flexmock()
+        (
+            flexmock(adapter.connection)
+            .should_receive('create_bucket')
+            .with_args('test-bucket')
+            .and_return(fake_bucket)
+            .once()
+        )
+        bucket = adapter._get_or_create_bucket()
+        assert bucket is fake_bucket
+
+    def test_get_or_create_bucket_fails_on_permission_error(self):
+        from boto.exception import S3ResponseError
+
+        adapter = make_adapter('TEST_ID', 'TEST_SECRET', 'test-bucket')
+        (
+            flexmock(adapter.connection)
+            .should_receive('get_bucket')
+            .with_args('test-bucket')
+            .and_raise(S3ResponseError, 403, 'Forbidden', '')
+            .once()
+        )
+        (
+            flexmock(adapter.connection)
+            .should_receive('create_bucket')
+            .never()
+        )
+        with pytest.raises(S3ResponseError) as exc:
+            adapter._get_or_create_bucket()
+
+    def test_bucket_delegates_to_get_or_create_bucket(self):
+        adapter = make_adapter('TEST_ID', 'TEST_SECRET', 'test-bucket')
+        fake_bucket = flexmock()
+        (
+            flexmock(adapter)
+            .should_receive('_get_or_create_bucket')
+            .and_return(fake_bucket)
+            .once()
+        )
+        assert adapter.bucket is fake_bucket
+        assert adapter._bucket is fake_bucket
+
+    def test_bucket_uses_cached_bucket_if_present(self):
+        adapter = make_adapter('TEST_ID', 'TEST_SECRET', 'test-bucket')
+        adapter._bucket = flexmock()
+        (
+            flexmock(adapter)
+            .should_receive('_get_or_create_bucket')
+            .never()
+        )
+        assert adapter.bucket is adapter._bucket
+
+TestAmazonS3 = pytest.mark.unit(TestAmazonS3)
---
+++ unistorage/adapters/amazon.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+    unistorage.adapters.amazon
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    This module an adapter for Amazon Simple Storage Service (S3).
+
+    :copyright: (c) 2012 by Janne Vanhala.
+    :license: BSD, see LICENSE for more details.
+
+"""
+from unistorage.exceptions import FileNotFound
+from unistorage.interface import Adapter
+
+
+class AmazonS3(Adapter):
+    """
+    An adapter for Amazon Simple Storage Service (S3).
+
+    This adapter requires an boto_ library to be installed. You can
+    install it by executing the following command in the terminal::
+
+        pip install boto
+
+    .. _boto: https://github.com/boto/boto
+    """
+
+    def __init__(self, access_key, secret_key, bucket_name):
+        """
+        Construct a Amazon S3 adapter.
+
+        :param access_key: your Amazon Web Services access key id
+        :param secret_key: your Amazon Web Services secret access key
+        :param bucket_name: your Amazon S3 bucket name as a string
+        """
+        boto = self._import_boto()
+        self.connection = boto.connect_s3(access_key, secret_key)
+        self.bucket_name = bucket_name
+
+    @property
+    def bucket(self):
+        """
+        The :class:`boto.s3.Bucket` instance with the name
+        :attr:`bucket_name`.
+        """
+        if not hasattr(self, '_bucket'):
+            self._bucket = self._get_or_create_bucket()
+        return self._bucket
+
+    def _get_or_create_bucket(self):
+        """
+        Retrive the bucket with the name :attr:`bucket_name`, and create
+        one if necessary.
+
+        :return: a :class:`boto.s3.Bucket` instance
+        """
+        from boto.exception import S3ResponseError
+        try:
+            bucket = self.connection.get_bucket(self.bucket_name)
+        except S3ResponseError as exc:
+            if exc.status != 404:
+                raise
+            bucket = self.connection.create_bucket(self.bucket_name)
+        return bucket
+
+    def _import_boto(self):
+        """
+        Import and return :module:`boto`.
+
+        If the module cannot be imported, for example due to it not being
+        installed, a :exception:`RuntimeError` is raised.
+        """
+        try:
+            import boto
+        except ImportError:
+            raise RuntimeError(
+                'Could not import boto. Amazon S3 adapter requires boto '
+                'library to be installed. You can install it by executing '
+                '``pip install boto`` in the terminal.'
+            )
+        return boto
+
+    def delete(self, name):
+        key = self.bucket.get_key(name)
+        if not key:
+            raise FileNotFound(name)
+        return key.delete()
+
+    def exists(self, name):
+        return self.bucket.get_key(name) is not None
+
+    def write(self, name, content):
+        key = self.bucket.new_key(name)
+        key.set_contents_from_string(content)
+
+    def read(self, name):
+        key = self.bucket.get_key(name)
+        if not key:
+            raise FileNotFound(name)
+        return key.get_contents_as_string()
+
+    def size(self, name):
+        key = self.bucket.get_key(name)
+        if not key:
+            raise FileNotFound(name)
+        metadata = key.get_metadata()
+        return metadata
