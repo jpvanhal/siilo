dd280a13382e6e7cc71a5a7287f28f46d7d85d01
Janne Vanhala <janne.vanhala@gmail.com>
2012-10-13 17:27:13 +0300

    fixed a problemn in amazon s3 functional tests due to a delay in bucket creation that would make the tests fail randomly.

--- tests/functional/adapters/test_amazon.py
+++ tests/functional/adapters/test_amazon.py
@@ -1,4 +1,5 @@
 import os
+import time
 import uuid
 
 import pytest
@@ -18,14 +19,45 @@ class TestAmazonS3(FunctionalTestCase):
         except KeyError:
             pytest.skip()
 
-        return AmazonS3(
+        adapter = AmazonS3(
             access_key=access_key,
             secret_key=secret_key,
             bucket_name='unistorage-test-%s' % uuid.uuid4()
         )
+
+        self._ensure_bucket_exists(adapter)
+
+        return adapter
 
     def teardown_adapter(self, adapter):
         bucket = adapter.bucket
         for key in bucket.list():
             key.delete()
         adapter.connection.delete_bucket(bucket.name)
+
+    def _ensure_bucket_exists(self, adapter):
+        """
+        Create the bucket specified in the given adapter, and ensure it
+        exists before returning.
+
+        There is sometimes a short delay before the bucket is actually
+        created in AmazonS3 after calling
+        :method:`boto.s3.bucket.Bucket.create_bucket`. Thus the Amazon
+        S3 tests could fail randomly due to the bucket being found. This
+        method works around the problem by blocking until the bucket
+        truly exists.
+
+        :param adapter: a :class:`unistorage.adapters.amazon.AmazonS3` instance
+        """
+        from boto.exception import S3ResponseError
+
+        adapter.connection.create_bucket(adapter.bucket_name)
+        while True:
+            try:
+                adapter.connection.get_bucket(adapter.bucket_name)
+            except S3ResponseError as exc:
+                if exc.status != 404:
+                    raise
+                time.sleep(0.5)
+            else:
+                break
