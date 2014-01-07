b9a328e0651ad8d5e6210a7c6c93011a1d535d60
Janne Vanhala <janne.vanhala@gmail.com>
2014-01-07 22:05:19 +0200

    Redesign the adapter interface

--- silo/interface.py
+++ silo/interface.py
@@ -10,44 +10,15 @@
 
 class Adapter(object):
 
-    def accessed(self, name):
-        """
-        Return the last accessed time of a file.
-
-        For storage systems that are not able to return the last
-        accessed time this will raise :exc:`NotImplementedError`
-        instead.
-
-        If a file with the given `name` does not exist then a
-        :exc:`storage.exc.FileNotFoundError` exception is raised.
-
-        :argument name: the name of the file whose last accessed time
-          you want to check.
-        :returns: a :class:`datetime.datetime` object
-        """
-        raise NotImplementedError
-
-    def created(self, name):
-        """
-        Return the creation time of a file.
-
-        For storage systems that are not able to return the creation
-        time this will raise :exc:`NotImplementedError` instead.
-
-        If a file with the given `name` does not exist then a
-        :exc:`storage.exc.FileNotFoundError` exception is raised.
-
-        :argument name: the name of the file whose creation time you
-          want to check.
-        :returns: a :class:`datetime.datetime` object
-        """
-        raise NotImplementedError
-
     def delete(self, name):
         """
         Delete the file referenced by ``name``.
 
-        :argument name: the name of the file to be deleted
+        :param str name: the name of the file to be deleted
+        :raises storage.exc.FileNotFoundError: if a file with the given
+          `name` does not exist
+        :raises NotImplementedError: if the storage system does not
+          support file deletion
         """
         raise NotImplementedError
 
@@ -55,45 +26,23 @@ class Adapter(object):
         """
         Check for the existence of a file.
 
-        :argument name: the name of the file to check for existence
-        :returns: ``True`` if `name` refers to an existing file within
+        :param str name: the name of the file to check for existence
+        :return: ``True`` if `name` refers to an existing file within
           this storage, or ``False`` if the name is available for a new
           file.
+        :rtype: bool
         """
         raise NotImplementedError
 
-    def list(self):
+    def open(self, name, mode='rb'):
         """
-        Yield the name of each file the storage contains.
-        """
-        raise NotImplementedError
+        Open a file.
 
-    def modified(self, name):
-        """
-        Return the last modified time of a file.
-
-        If a file with the given `name` does not exist then a
-        :exc:`storage.exc.FileNotFoundError` exception is raised.
-
-        For storage systems that are not able to return the last
-        modified time this will raise :exc:`NotImplementedError`
-        instead.
-
-        :argument name: the name of the file whose last modified time
-          you want to check.
-        :returns: a :class:`datetime.datetime` object
-        """
-        raise NotImplementedError
-
-    def read(self, name):
-        """
-        Read the contents of a file.
-
-        If a file with the given `name` does not exist then a
-        :exc:`storage.exc.FileNotFoundError` exception is raised.
-
-        :argument name: the name of the file to retrieve
-        :returns: a string representing the file contents
+        :param str name: the name of the file to be opened
+        :param str mode: a string indicating how the file should be
+          opened
+        :raises storage.exc.FileNotFoundError: if a file with the given
+          `name` does not exist
         """
         raise NotImplementedError
 
@@ -101,26 +50,13 @@ class Adapter(object):
         """
         Return the size of a file in bytes.
 
-        If a file with the given `name` does not exist then a
-        :exc:`storage.exc.FileNotFoundError` exception is raised.
-
-        For storage systems that are not able to return the file size
-        this will raise :exc:`NotImplementedError` instead.
-
-        :argument name: the name of the file
-        :returns: an integer representing the size of the file in bytes
-        """
-        raise NotImplementedError
-
-    def write(self, name, content):
-        """
-        Write the given ``content`` to a file.
-
-        If there already exists a file with the name ``name``, this
-        method will overwrite the file.
-
-        :argument name: the name of the file
-        :argument content: a string to be written to the file
+        :param str name: the name of the file
+        :return: the size of the file in bytes
+        :rtype: int
+        :raises storage.exc.FileNotFoundError: if a file with the given
+          `name` does not exist
+        :raises NotImplementedError: if the storage system is not able
+          to return the file size
         """
         raise NotImplementedError
 
@@ -129,10 +65,12 @@ class Adapter(object):
         Return the URL where the file referenced by ``name`` can be
         accessed.
 
-        For storage systems that do not support access by URL this will
-        raise :exc:`NotImplementedError` instead.
-
-        :argument name: the name of the file
-        :returns: a string representing the URL of the file
+        :param str name: the name of the file
+        :return: the URL where file can be accessed
+        :rtype: str
+        :raises storage.exc.FileNotFoundError: if a file with the given
+          `name` does not exist
+        :raises NotImplementedError: if the storage system does not
+          support access by URL
         """
         raise NotImplementedError
