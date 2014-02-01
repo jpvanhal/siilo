0805d1c1c299d518ede905be8446cac197637d8d
Janne Vanhala <janne.vanhala@gmail.com>
2014-02-01 22:03:25 +0200

    Refactor tests

--- tests/storages/test_base.py
+++ tests/storages/test_base.py
@@ -1,28 +1,32 @@
 import pytest
 
 
-class TestStorage(object):
-    @pytest.fixture
-    def storage(self):
-        from silo.storages import Storage
-        return Storage()
+@pytest.fixture
+def storage():
+    from silo.storages import Storage
+    return Storage()
 
-    def test_delete_raises_not_implemented_error(self, storage):
-        with pytest.raises(NotImplementedError):
-            storage.delete('README.rst')
 
-    def test_exists_raises_not_implemented_error(self, storage):
-        with pytest.raises(NotImplementedError):
-            storage.exists('README.rst')
+def test_delete_raises_not_implemented_error(storage):
+    with pytest.raises(NotImplementedError):
+        storage.delete('README.rst')
 
-    def test_open_raises_not_implemented_error(self, storage):
-        with pytest.raises(NotImplementedError):
-            storage.open('README.rst')
 
-    def test_size_raises_not_implemented_error(self, storage):
-        with pytest.raises(NotImplementedError):
-            storage.size('README.rst')
+def test_exists_raises_not_implemented_error(storage):
+    with pytest.raises(NotImplementedError):
+        storage.exists('README.rst')
 
-    def test_url_raises_not_implemented_error(self, storage):
-        with pytest.raises(NotImplementedError):
-            storage.url('README.rst')
+
+def test_open_raises_not_implemented_error(storage):
+    with pytest.raises(NotImplementedError):
+        storage.open('README.rst')
+
+
+def test_size_raises_not_implemented_error(storage):
+    with pytest.raises(NotImplementedError):
+        storage.size('README.rst')
+
+
+def test_url_raises_not_implemented_error(storage):
+    with pytest.raises(NotImplementedError):
+        storage.url('README.rst')
--- tests/storages/test_filesystem.py
+++ tests/storages/test_filesystem.py
@@ -6,79 +6,93 @@ import pytest
 from silo.exceptions import FileNotFoundError, FileNotWithinStorageError
 
 
-class TestFileSystemStorage(object):
-    @pytest.fixture
-    def storage(self, tmpdir):
-        from silo.storages.filesystem import FileSystemStorage
-        return FileSystemStorage(directory=str(tmpdir))
+@pytest.fixture
+def storage(tmpdir):
+    from silo.storages.filesystem import FileSystemStorage
+    return FileSystemStorage(directory=str(tmpdir))
 
-    def test_constructor_sets_directory(self, storage, tmpdir):
-        assert storage.directory == str(tmpdir)
 
-    def test_relative_directory_is_normalized(self, storage):
-        storage.directory = 'foobar'
-        assert storage.directory == os.path.abspath('foobar')
+def test_constructor_sets_directory(storage, tmpdir):
+    assert storage.directory == str(tmpdir)
 
-    def test_delete_removes_the_file(self, storage, tmpdir):
-        file_ = tmpdir.join('foobar').ensure()
+
+def test_relative_directory_is_normalized(storage):
+    storage.directory = 'foobar'
+    assert storage.directory == os.path.abspath('foobar')
+
+
+def test_delete_removes_the_file(storage, tmpdir):
+    file_ = tmpdir.join('foobar').ensure()
+    storage.delete('foobar')
+    assert not file_.check()
+
+
+def test_delete_raises_error_if_file_doesnt_exist(storage):
+    with pytest.raises(FileNotFoundError) as excinfo:
         storage.delete('foobar')
-        assert not file_.check()
+    assert excinfo.value.name == 'foobar'
 
-    def test_delete_raises_error_if_file_doesnt_exist(self, storage):
-        with pytest.raises(FileNotFoundError) as excinfo:
-            storage.delete('foobar')
-        assert excinfo.value.name == 'foobar'
 
-    def test_delete_raises_error_if_file_not_within_storage(self, storage):
-        with pytest.raises(FileNotWithinStorageError) as excinfo:
-            storage.delete('../foobar')
-        assert excinfo.value.name == '../foobar'
+def test_delete_raises_error_if_file_not_within_storage(storage):
+    with pytest.raises(FileNotWithinStorageError) as excinfo:
+        storage.delete('../foobar')
+    assert excinfo.value.name == '../foobar'
 
-    def test_exists_returns_false_if_file_doesnt_exist(self, storage):
-        assert storage.exists('foobar') is False
 
-    def test_exists_returns_true_if_file_exists(self, storage, tmpdir):
-        tmpdir.join('foobar').ensure()
-        assert storage.exists('foobar') is True
+def test_exists_returns_false_if_file_doesnt_exist(storage):
+    assert storage.exists('foobar') is False
 
-    def test_exists_raises_error_if_file_not_within_storage(self, storage):
-        with pytest.raises(FileNotWithinStorageError) as excinfo:
-            storage.exists('../foobar')
-        assert excinfo.value.name == '../foobar'
 
-    def test_size_returns_file_size_in_bytes(self, storage, tmpdir):
-        tmpdir.join('foobar').write('xyzzy')
-        assert storage.size('foobar') == 5
+def test_exists_returns_true_if_file_exists(storage, tmpdir):
+    tmpdir.join('foobar').ensure()
+    assert storage.exists('foobar') is True
 
-    def test_size_raises_error_if_file_doesnt_exist(self, storage):
-        with pytest.raises(FileNotFoundError) as excinfo:
-            storage.size('foobar')
-        assert excinfo.value.name == 'foobar'
 
-    def test_size_raises_error_if_file_not_within_storage(self, storage):
-        with pytest.raises(FileNotWithinStorageError) as excinfo:
-            storage.size('../foobar')
-        assert excinfo.value.name == '../foobar'
+def test_exists_raises_error_if_file_not_within_storage(storage):
+    with pytest.raises(FileNotWithinStorageError) as excinfo:
+        storage.exists('../foobar')
+    assert excinfo.value.name == '../foobar'
 
-    def test_open_returns_open_file_with_default_mode(self, storage, tmpdir):
-        tmpdir.join('foobar').ensure()
-        file_ = storage.open('foobar')
-        assert isinstance(file_, io.IOBase)
-        assert file_.name == str(tmpdir.join('foobar'))
-        assert file_.mode == 'rb'
 
-    def test_open_returns_open_file_with_given_mode(self, storage, tmpdir):
-        file_ = storage.open('foobar', 'w')
-        assert isinstance(file_, io.IOBase)
-        assert file_.name == str(tmpdir.join('foobar'))
-        assert file_.mode == 'w'
+def test_size_returns_file_size_in_bytes(storage, tmpdir):
+    tmpdir.join('foobar').write('xyzzy')
+    assert storage.size('foobar') == 5
 
-    def test_open_raises_error_if_file_doesnt_exist(self, storage):
-        with pytest.raises(FileNotFoundError) as excinfo:
-            storage.open('foobar', 'r')
-        assert excinfo.value.name == 'foobar'
 
-    def test_open_raises_error_if_file_not_within_storage(self, storage):
-        with pytest.raises(FileNotWithinStorageError) as excinfo:
-            storage.open('../foobar')
-        assert excinfo.value.name == '../foobar'
+def test_size_raises_error_if_file_doesnt_exist(storage):
+    with pytest.raises(FileNotFoundError) as excinfo:
+        storage.size('foobar')
+    assert excinfo.value.name == 'foobar'
+
+
+def test_size_raises_error_if_file_not_within_storage(storage):
+    with pytest.raises(FileNotWithinStorageError) as excinfo:
+        storage.size('../foobar')
+    assert excinfo.value.name == '../foobar'
+
+
+def test_open_returns_open_file_with_default_mode(storage, tmpdir):
+    tmpdir.join('foobar').ensure()
+    file_ = storage.open('foobar')
+    assert isinstance(file_, io.IOBase)
+    assert file_.name == str(tmpdir.join('foobar'))
+    assert file_.mode == 'rb'
+
+
+def test_open_returns_open_file_with_given_mode(storage, tmpdir):
+    file_ = storage.open('foobar', 'w')
+    assert isinstance(file_, io.IOBase)
+    assert file_.name == str(tmpdir.join('foobar'))
+    assert file_.mode == 'w'
+
+
+def test_open_raises_error_if_file_doesnt_exist(storage):
+    with pytest.raises(FileNotFoundError) as excinfo:
+        storage.open('foobar', 'r')
+    assert excinfo.value.name == 'foobar'
+
+
+def test_open_raises_error_if_file_not_within_storage(storage):
+    with pytest.raises(FileNotWithinStorageError) as excinfo:
+        storage.open('../foobar')
+    assert excinfo.value.name == '../foobar'
